ea67c34a703adc42c1eeade6267d4e4143acffbc
Kayros Andrade <112031502+KayrosAndrade@users.noreply.github.com>
2022-08-26 22:37:18 -0300

    Atualizando

--- questao1.js
+++ questao1.js
@@ -1,7 +1,7 @@
 const l1 =parseFloat(prompt("Digite o valor do 1ª lado do triângulo:"));
 const l2 =parseFloat(prompt("Digite o valor do 2ª lado do triângulo:"));
 const l3 =parseFloat(prompt("Digite o valor do 3ª lado do triângulo:"));
-    if(l1<(l2+l3)&&l2<(l1+l3)&&l3<(l1+l2)){
+    if(l1 > 0 && l2 > 0 && l3 > 0){
         if(l1 == l2 && l2 == l3){
             alert("O triângulo é equilátero");
         }else if(l1 == l2 || l1 == l3 || l2 == l3){
